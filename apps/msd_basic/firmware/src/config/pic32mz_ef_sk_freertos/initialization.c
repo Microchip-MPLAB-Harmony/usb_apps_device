d57bca7dc18085bc31a0d4b0d97ba20ad7f4a9e1
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 18:37:37 +0530

    [APPS]: Regeneration of the demo msd_basic/firmware/pic32mz_ef_sk_freertos.X [MH3-54546].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/msd_basic/firmware/src/config/pic32mz_ef_sk_freertos/initialization.c
+++ apps/msd_basic/firmware/src/config/pic32mz_ef_sk_freertos/initialization.c
@@ -231,6 +231,7 @@ const DRV_USBHS_INIT drvUSBInit =
 
 void SYS_Initialize ( void* data )
 {
+
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
