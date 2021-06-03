024285a167838ac068c3398fa43b525f71386aef
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 15:50:05 +0530

    [APPS]: Regeneration of the demo cdc_com_port_single/firmware/sam_a5d2_xult_iar.IAR [MH3-54476].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/cdc_com_port_single/firmware/src/config/sam_a5d2_xult_iar/device.h
+++ apps/cdc_com_port_single/firmware/src/config/sam_a5d2_xult_iar/device.h
@@ -41,7 +41,17 @@
 *******************************************************************************/
 // DOM-IGNORE-END
 
-#include "atsama5d27.h"
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#ifndef DONT_USE_PREDEFINED_CORE_HANDLERS
+    #define DONT_USE_PREDEFINED_CORE_HANDLERS
+#endif //DONT_USE_PREDEFINED_CORE_HANDLERS
+#ifndef DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
+    #define DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
+#endif //DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
+#include "sama5d27.h"
 #include "device_cache.h"
 #include "toolchain_specifics.h"
 
+#endif //DEVICE_H
