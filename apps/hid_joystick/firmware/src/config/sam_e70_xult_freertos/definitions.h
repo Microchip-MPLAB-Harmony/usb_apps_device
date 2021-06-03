bb1c69c484e2450ed8fb0ff78d158aecfe3e5e32
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 17:54:19 +0530

    [APPS]: Regeneration of the demo hid_joystick/firmware/sam_e70_xult_freertos.X [MH3-54528].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/hid_joystick/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/hid_joystick/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -76,6 +76,9 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 300000000
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System Functions
@@ -168,22 +171,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
 	SYS_MODULE_OBJ  drvUSBHSV1Object;
