b3a08e8069765ce851e93bc87310e081f6d032fd
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 12:22:16 +0530

    [APPS]: Regenerating the demo msd_sdcard/firmware/sam_a5d27_wlsom1_ek1.X [MH3-62149].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/msd_sdcard/firmware/src/config/sam_a5d27_wlsom1_ek1/peripheral/pio/plib_pio.c
+++ apps/msd_sdcard/firmware/src/config/sam_a5d27_wlsom1_ek1/peripheral/pio/plib_pio.c
@@ -80,11 +80,15 @@ void PIO_Initialize ( void )
 	PIOA_REGS->PIO_MSKR = 0x10000U;
 	PIOA_REGS->PIO_CFGR = (PIOA_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x3400U;
 	
+ /* Port A Latch configuration */
+	PIOA_REGS->PIO_CODR = 0x101c0U;
 
  /* Port B Peripheral function GPIO configuration */
 	PIOB_REGS->PIO_MSKR = 0x4U;
 	PIOB_REGS->PIO_CFGR = 0x0U;
 	
+ /* Port B Latch configuration */
+	PIOB_REGS->PIO_CODR = 0x4U;
 
 
 
