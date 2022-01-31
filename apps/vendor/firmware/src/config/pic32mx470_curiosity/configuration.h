05e419433ceeb81434ea930cc70eaf27dc0ee356
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 12:47:18 +0530

    [APPS]: Regenerating the demo vendor/firmware/pic32mx470_curiosity.X [MH3-62161].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/vendor/firmware/src/config/pic32mx470_curiosity/configuration.h
+++ apps/vendor/firmware/src/config/pic32mx470_curiosity/configuration.h
@@ -56,7 +56,7 @@
 */
 
 #include "user.h"
-#include "toolchain_specifics.h"
+#include "device.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -127,9 +127,6 @@ extern "C" {
 
 /* Enable SOF Events */
 #define USB_DEVICE_SOF_EVENT_ENABLE
-
-
-
 
 
 /* Endpoint Transfer Queue Size for both read and
