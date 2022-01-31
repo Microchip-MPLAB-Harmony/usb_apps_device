3e775d03d59c507c9228353a10befc658d945db4
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 09:59:57 +0530

    [APPS]: Regenerating the demo cdc_msd_sdcard/firmware/pic32mz_ef_curiosity_2_0.X [MH3-62077].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/cdc_msd_sdcard/firmware/src/config/pic32mz_ef_curiosity_2_0/peripheral/gpio/plib_gpio.c
+++ apps/cdc_msd_sdcard/firmware/src/config/pic32mz_ef_curiosity_2_0/peripheral/gpio/plib_gpio.c
@@ -57,43 +57,35 @@
 */
 void GPIO_Initialize ( void )
 {
-    /* PORTA Initialization */
 
+    /* PORTA Initialization */
     /* PORTB Initialization */
     ODCBSET = 0x380; /* Open Drain Enable */
     LATB = 0x380; /* Initial Latch Value */
     TRISBCLR = 0x380; /* Direction Control */
-
     /* PORTC Initialization */
-
     /* PORTD Initialization */
     LATD = 0x200; /* Initial Latch Value */
     TRISDCLR = 0x200; /* Direction Control */
     ANSELDCLR = 0x4000; /* Digital Mode Enable */
-
     /* PORTE Initialization */
-
     /* PORTF Initialization */
-
     /* PORTG Initialization */
-
     /* PORTH Initialization */
-
     /* PORTJ Initialization */
     ODCJSET = 0x88; /* Open Drain Enable */
     LATJ = 0x88; /* Initial Latch Value */
     TRISJCLR = 0x88; /* Direction Control */
-
     /* PORTK Initialization */
     ODCKSET = 0x80; /* Open Drain Enable */
     LATK = 0x80; /* Initial Latch Value */
     TRISKCLR = 0x80; /* Direction Control */
 
-
     /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
+
     CFGCONbits.IOLOCK = 0;
 
     /* PPS Input Remapping */
@@ -102,8 +94,9 @@ void GPIO_Initialize ( void )
     /* PPS Output Remapping */
     RPD7R = 5;
 
-    /* Lock back the system after PPS configuration */
+        /* Lock back the system after PPS configuration */
     CFGCONbits.IOLOCK = 1;
+
     SYSKEY = 0x00000000;
 
 }
