24352a7fa625fcd43ab5dbda7c451ab014b81200
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 08:21:19 +0530

    [APPS]: Regenerating the demo cdc_com_port_dual/firmware/pic32cm_le00_curiosity_pro.X [MH3-62024].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/cdc_com_port_dual/firmware/src/config/pic32cm_le00_curiosity_pro/configuration.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32cm_le00_curiosity_pro/configuration.h
@@ -106,10 +106,6 @@ extern "C" {
 #define USB_DEVICE_EP0_BUFFER_SIZE                          64
 
 
-
-
-
-
 /* Maximum instances of CDC function driver */
 #define USB_DEVICE_CDC_INSTANCES_NUMBER                     2
 
