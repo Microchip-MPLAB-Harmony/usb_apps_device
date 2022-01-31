fcc8350c76de8c0f65e39c1944a223e6971aac5b
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 10:29:27 +0530

    [APPS]: Regenerating the demo hid_basic/firmware/pic32mx470_curiosity.X [MH3-62093].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/hid_basic/firmware/src/config/pic32mx470_curiosity/configuration.h
+++ apps/hid_basic/firmware/src/config/pic32mx470_curiosity/configuration.h
@@ -56,7 +56,7 @@
 */
 
 #include "user.h"
-#include "toolchain_specifics.h"
+#include "device.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -135,9 +135,6 @@ extern "C" {
 
 /* Enable SOF Events */
 #define USB_DEVICE_SOF_EVENT_ENABLE
-
-
-
 
 
 
