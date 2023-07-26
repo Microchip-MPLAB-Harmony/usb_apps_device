99e053abb9d6ad0051ef7ab3530c459eab76e6f8
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 22:07:50 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_pic32mx470_curiosity
    MH3-82184

--- apps/cdc_com_port_dual/firmware/src/config/pic32mx470_curiosity/definitions.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32mx470_curiosity/definitions.h
@@ -72,6 +72,12 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MX470F512H"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MX1185"
+#define DEVICE_SERIES		 "PIC32MX"
+
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 96000000
 
@@ -185,7 +191,7 @@ Remarks:
 
 typedef struct
 {
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
 	SYS_MODULE_OBJ  drvUSBFSObject;
 
--- apps/cdc_com_port_dual/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/cdc_com_port_dual/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
@@ -42,6 +42,7 @@
 //DOM-IGNORE-END
 
 #include "plib_gpio.h"
+#include "interrupts.h"
 
 
 
@@ -115,7 +116,7 @@ void GPIO_Initialize ( void )
 */
 uint32_t GPIO_PortRead(GPIO_PORT port)
 {
-    return (*(volatile uint32_t *)(&PORTB + (port * 0x40)));
+    return (*(volatile uint32_t *)(&PORTB + (port * 0x40U)));
 }
 
 // *****************************************************************************
@@ -130,7 +131,7 @@ uint32_t GPIO_PortRead(GPIO_PORT port)
 */
 void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value)
 {
-    *(volatile uint32_t *)(&LATB + (port * 0x40)) = (*(volatile uint32_t *)(&LATB + (port * 0x40)) & (~mask)) | (mask & value);
+    *(volatile uint32_t *)(&LATB + (port * 0x40U)) = (*(volatile uint32_t *)(&LATB + (port * 0x40U)) & (~mask)) | (mask & value);
 }
 
 // *****************************************************************************
@@ -145,7 +146,7 @@ void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value)
 */
 uint32_t GPIO_PortLatchRead(GPIO_PORT port)
 {
-    return (*(volatile uint32_t *)(&LATB + (port * 0x40)));
+    return (*(volatile uint32_t *)(&LATB + (port * 0x40U)));
 }
 
 // *****************************************************************************
@@ -160,7 +161,7 @@ uint32_t GPIO_PortLatchRead(GPIO_PORT port)
 */
 void GPIO_PortSet(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&LATBSET + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&LATBSET + (port * 0x40U)) = mask;
 }
 
 // *****************************************************************************
@@ -175,7 +176,7 @@ void GPIO_PortSet(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortClear(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&LATBCLR + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&LATBCLR + (port * 0x40U)) = mask;
 }
 
 // *****************************************************************************
@@ -190,7 +191,7 @@ void GPIO_PortClear(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortToggle(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&LATBINV + (port * 0x40))= mask;
+    *(volatile uint32_t *)(&LATBINV + (port * 0x40U))= mask;
 }
 
 // *****************************************************************************
@@ -205,7 +206,7 @@ void GPIO_PortToggle(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&TRISBSET + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&TRISBSET + (port * 0x40U)) = mask;
 }
 
 // *****************************************************************************
@@ -220,7 +221,7 @@ void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&TRISBCLR + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&TRISBCLR + (port * 0x40U)) = mask;
 }
 
 
