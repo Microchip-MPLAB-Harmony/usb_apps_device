ab0abe5a52768542c45600ae20087997fdfa78c1
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 23:19:03 +0530

    [Apps.] Regenerated demo for cdc_com_port_single_sam_e70_xult_freertos
    MH3-82230

--- apps/cdc_com_port_single/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/cdc_com_port_single/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -76,6 +76,12 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME70Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME70"
+
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
 
@@ -191,7 +197,7 @@ typedef struct
 {
 	SYS_MODULE_OBJ  drvUSBHSV1Object;
 
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
 
 } SYSTEM_OBJECTS;
