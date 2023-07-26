77115cb0df1b1b4396a94bb8e1b78aa13395a1cc
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 23:55:11 +0530

    [Apps.] Regenerated demo for vendor_sam_a5d27_som1_ek
    MH3-82364

--- apps/vendor/firmware/src/packs/ATSAMA5D27CD1G_DFP/instance/aes.h
+++ apps/vendor/firmware/src/packs/ATSAMA5D27CD1G_DFP/instance/aes.h
@@ -1,7 +1,7 @@
 /*
- * Instance header file for ATSAMA5D28CLD2G
+ * Instance header file for ATSAMA5D29
  *
- * Copyright (c) 2021 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,15 +20,15 @@
  *
  */
 
-/* file generated from device description version 2021-06-28T09:00:02Z */
-#ifndef _SAMA5D2_AES_INSTANCE_
-#define _SAMA5D2_AES_INSTANCE_
+/* file generated from device description version 2022-10-04T10:37:14Z */
+#ifndef _SAMA5D_AES_INSTANCE_
+#define _SAMA5D_AES_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for AES peripheral ========== */
-#define AES_INSTANCE_ID                          _UL_(9)    
-#define AES_CLOCK_ID                             _UL_(9)    
-#define AES_DMAC_ID_TX                           _UL_(26)   
-#define AES_DMAC_ID_RX                           _UL_(27)   
+#define AES_INSTANCE_ID                          (9)        
+#define AES_CLOCK_ID                             (9)        
+#define AES_DMAC_ID_TX                           (26)       
+#define AES_DMAC_ID_RX                           (27)       
 
-#endif /* _SAMA5D2_AES_INSTANCE_ */
+#endif /* _SAMA5D_AES_INSTANCE_ */
