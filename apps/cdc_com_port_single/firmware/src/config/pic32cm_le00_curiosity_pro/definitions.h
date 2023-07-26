8d721c4382919b82d167b1e5df8157aba2365313
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 21:41:34 +0530

    [Apps.] Regenerated demo for cdc_com_port_single_pic32cm_le00_curiosity_pro
    MH3-82199

--- apps/cdc_com_port_single/firmware/src/config/pic32cm_le00_curiosity_pro/definitions.h
+++ apps/cdc_com_port_single/firmware/src/config/pic32cm_le00_curiosity_pro/definitions.h
@@ -56,10 +56,10 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/pm/plib_pm.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
 #include "driver/usb/usbfsv1/drv_usbfsv1.h"
+#include "peripheral/pm/plib_pm.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
@@ -74,6 +74,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CM5164LE00100"
+#define DEVICE_ARCH			 "CORTEX-M23"
+#define DEVICE_FAMILY		 "PIC32CM"
+#define DEVICE_SERIES		 "PIC32CMLE00"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
