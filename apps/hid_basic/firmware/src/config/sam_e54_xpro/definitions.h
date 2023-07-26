48689439506145492412c3a071072012cbd64e00
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 22:58:52 +0530

    [Apps.] Regenerated demo for hid_basic_sam_e54_xpro
    MH3-82282

--- apps/hid_basic/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/hid_basic/firmware/src/config/sam_e54_xpro/definitions.h
@@ -57,9 +57,9 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
@@ -75,6 +75,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME54P20A"
+#define DEVICE_ARCH			 "CORTEX-M4"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME54"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 120000000
@@ -189,9 +195,9 @@ Remarks:
 
 typedef struct
 {
-	SYS_MODULE_OBJ  drvUSBFSV1Object;
+    SYS_MODULE_OBJ  usbDevObject0;
 
-	SYS_MODULE_OBJ  usbDevObject0;
+	SYS_MODULE_OBJ  drvUSBFSV1Object;
 
 
 } SYSTEM_OBJECTS;
