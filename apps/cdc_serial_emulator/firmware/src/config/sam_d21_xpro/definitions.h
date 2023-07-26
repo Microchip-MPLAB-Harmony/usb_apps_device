30131cb8faf8a626a2f7febcee368c3ba2b4bf2b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 22:49:23 +0530

    [Apps.] Regenerated demo for cdc_serial_emulator_sam_d21_xpro
    MH3-82253

--- apps/cdc_serial_emulator/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/cdc_serial_emulator/firmware/src/config/sam_d21_xpro/definitions.h
@@ -77,6 +77,12 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMD21J18A"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAMD"
+#define DEVICE_SERIES		 "SAMD21"
+
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
 
@@ -190,7 +196,7 @@ Remarks:
 
 typedef struct
 {
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
     SYS_MODULE_OBJ  drvUsart0;
 	SYS_MODULE_OBJ  drvUSBFSV1Object;
--- apps/cdc_serial_emulator/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
+++ apps/cdc_serial_emulator/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -72,25 +71,27 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_OUT = 0x8000;
-   PORT_REGS->GROUP[0].PORT_PINCFG[14] = 0x6;
-   PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x6;
-   PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[24] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1;
+   PORT_REGS->GROUP[0].PORT_OUT = 0x8000U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[14] = 0x6U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x6U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[24] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[25] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
-   PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x66;
+   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0U;
+   PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22U;
+   PORT_REGS->GROUP[0].PORT_PMUX[12] = 0x66U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
-   PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
-   PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0;
+   PORT_REGS->GROUP[1].PORT_DIR = 0x40000000U;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x40000000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0U;
+
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0U;
 
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
