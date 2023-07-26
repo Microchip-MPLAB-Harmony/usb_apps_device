c63db1f3ae4f46df91f8a75d42dcef3a156dd709
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 22:33:06 +0530

    [Apps.] Regenerated demo for hid_basic_sam_9x75_eb
    MH3-82272

--- apps/hid_basic/firmware/src/packs/SAM9X75_DFP/instance/otpc.h
+++ apps/hid_basic/firmware/src/packs/SAM9X75_DFP/instance/otpc.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for ATSAM9X75D5M
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,14 +20,14 @@
  *
  */
 
-/* file generated from device description version 2022-01-05T08:57:53Z */
+/* file generated from device description version 2023-01-20T08:33:42Z */
 #ifndef _SAM9X7_OTPC_INSTANCE_
 #define _SAM9X7_OTPC_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for OTPC peripheral ========== */
-#define OTPC_INSTANCE_ID                         _UL_(46)   /* OTP Controller */
-#define OTPC_EMULATION_ADDRESS                   _UL_(0x00400000) 
-#define OTPC_EMULATION_SIZE                      _UL_(0x00001000) 
+#define OTPC_EMULATION_ADDRESS                   (0x00400000) 
+#define OTPC_EMULATION_SIZE                      (0x00001000) 
+#define OTPC_INSTANCE_ID                         (46)       /* OTP Controller */
 
 #endif /* _SAM9X7_OTPC_INSTANCE_ */
