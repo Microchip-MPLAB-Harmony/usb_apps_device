6932ab3c798a957d1abe897474a62b6ee708eb9f
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 21:58:55 +0530

    [Apps.] Regenerated demo for hid_basic_pic32mm_usb_curiosity
    MH3-82263

--- apps/hid_basic/firmware/src/config/pic32mm_usb_curiosity/configuration.h
+++ apps/hid_basic/firmware/src/config/pic32mm_usb_curiosity/configuration.h
@@ -101,26 +101,6 @@ extern "C" {
    function driver */
 #define USB_DEVICE_HID_QUEUE_DEPTH_COMBINED                 2
 
-/*** USB Driver Configuration ***/
-
-/* Maximum USB driver instances */
-#define DRV_USBFS_INSTANCES_NUMBER                        1
-
-/* Interrupt mode enabled */
-#define DRV_USBFS_INTERRUPT_MODE                          true
-
-
-/* Enables Device Support */
-#define DRV_USBFS_DEVICE_SUPPORT                          true
-	
-/* Disable Host Support */
-#define DRV_USBFS_HOST_SUPPORT                            false
-
-
-
-/* Alignment for buffers that are submitted to USB Driver*/ 
-#define USB_ALIGN  CACHE_ALIGN
-
 /* Number of Endpoints used */
 #define DRV_USBFS_ENDPOINTS_NUMBER                        3
 
@@ -136,6 +116,27 @@ extern "C" {
 /* Enable SOF Events */
 #define USB_DEVICE_SOF_EVENT_ENABLE
 
+
+/*** USB Driver Configuration ***/
+
+/* Maximum USB driver instances */
+#define DRV_USBFS_INSTANCES_NUMBER                        1
+
+/* Interrupt mode enabled */
+#define DRV_USBFS_INTERRUPT_MODE                          true
+
+
+/* Enables Device Support */
+#define DRV_USBFS_DEVICE_SUPPORT                          true
+
+/* Disable Host Support */
+#define DRV_USBFS_HOST_SUPPORT                            false
+
+
+
+
+/* Alignment for buffers that are submitted to USB Driver*/ 
+#define USB_ALIGN  CACHE_ALIGN
 
 
 
