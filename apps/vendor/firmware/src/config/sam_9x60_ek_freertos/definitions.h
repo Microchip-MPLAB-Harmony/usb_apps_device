13d06ec447c42141bfdf876735a0bc8d5d7bfb39
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 23:50:30 +0530

    [Apps.] Regenerated demo for vendor_sam_9x60_ek_freertos
    MH3-82362

--- apps/vendor/firmware/src/config/sam_9x60_ek_freertos/definitions.h
+++ apps/vendor/firmware/src/config/sam_9x60_ek_freertos/definitions.h
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/udphs/drv_usb_udphs.h"
 #include "bsp/bsp.h"
+#include "driver/usb/udphs/drv_usb_udphs.h"
 #include "peripheral/pit/plib_pit.h"
 #include "peripheral/mmu/plib_mmu.h"
 #include "peripheral/clk/plib_clk.h"
@@ -74,6 +74,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "SAM9X60"
+#define DEVICE_ARCH			 "ARM926EJ-S"
+#define DEVICE_FAMILY		 "SAM9"
+#define DEVICE_SERIES		 "SAM9X"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 600000000
@@ -190,7 +196,7 @@ typedef struct
 {
 	SYS_MODULE_OBJ  drvUSBUDPHSObject;
 
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
 
 } SYSTEM_OBJECTS;
