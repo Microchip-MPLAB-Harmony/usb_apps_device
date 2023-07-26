63084203585939724d269f84cfbb996f150ae854
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 23:21:28 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_sam_v71_xult
    MH3-82195

--- apps/cdc_com_port_dual/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/cdc_com_port_dual/firmware/src/config/sam_v71_xult/definitions.h
@@ -48,9 +48,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
-#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
@@ -73,6 +73,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMV71Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAMV"
+#define DEVICE_SERIES		 "SAMV71"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
@@ -189,7 +195,7 @@ typedef struct
 {
 	SYS_MODULE_OBJ  drvUSBHSV1Object;
 
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
 
 } SYSTEM_OBJECTS;
