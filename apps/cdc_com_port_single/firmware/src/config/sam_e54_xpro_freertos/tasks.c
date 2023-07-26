98a3c4d6410dae338919b8f05cfac692fca58417
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-26 23:04:00 +0530

    [Apps.] Regenerated demo for cdc_com_port_single_sam_e54_xpro_freertos
    MH3-82228

--- apps/cdc_com_port_single/firmware/src/config/sam_e54_xpro_freertos/tasks.c
+++ apps/cdc_com_port_single/firmware/src/config/sam_e54_xpro_freertos/tasks.c
@@ -52,6 +52,7 @@
 
 #include "configuration.h"
 #include "definitions.h"
+#include "sys_tasks.h"
 
 
 // *****************************************************************************
@@ -73,7 +74,7 @@ void _USB_DEVICE_Tasks(  void *pvParameters  )
 {
     while(1)
     {
-				 /* USB Device layer tasks routine */
+                /* USB Device layer tasks routine */
         USB_DEVICE_Tasks(sysObj.usbDevObject0);
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
@@ -82,12 +83,12 @@ void _USB_DEVICE_Tasks(  void *pvParameters  )
 /* Handle for the APP_FREERTOS_Tasks. */
 TaskHandle_t xAPP_FREERTOS_Tasks;
 
-void _APP_FREERTOS_Tasks(  void *pvParameters  )
+static void lAPP_FREERTOS_Tasks(  void *pvParameters  )
 {   
-    while(1)
+    while(true)
     {
         APP_FREERTOS_Tasks();
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 }
 
@@ -138,7 +139,7 @@ void SYS_Tasks ( void )
 
     /* Maintain the application's state machine. */
         /* Create OS Thread for APP_FREERTOS_Tasks. */
-    xTaskCreate((TaskFunction_t) _APP_FREERTOS_Tasks,
+    (void) xTaskCreate((TaskFunction_t) lAPP_FREERTOS_Tasks,
                 "APP_FREERTOS_Tasks",
                 1024,
                 NULL,
