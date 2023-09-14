8ebda6773bf5c382484b70a02ef918b018ac5e74
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-09-14 09:26:47 +0530

    [Apps.] Regenerated all PI32CZ CA80 and PIC32CK GC01 demo Configurations MH3-85675

--- apps/cdc_msd_sdcard/firmware/src/config/pic32cz_ca80_curiosity_ultra/driver/usb/usbhs/src/drv_usbhs.c
+++ apps/cdc_msd_sdcard/firmware/src/config/pic32cz_ca80_curiosity_ultra/driver/usb/usbhs/src/drv_usbhs.c
@@ -50,6 +50,16 @@
 #include "usb/src/usb_external_dependencies.h"
 #include "driver/usb/usbhs/src/drv_usbhs_local.h"
 
+/* Definitions */ 
+#define SUPC_VREGCTRL_AVREGEN_USBHS0_Pos             _UINT32_(16)
+#define SUPC_VREGCTRL_AVREGEN_USBHS1_Pos             _UINT32_(17)
+#define SUPC_VREGCTRL_AVREGEN_USBHS0_Msk             (_UINT32_(0x1) << SUPC_VREGCTRL_AVREGEN_USBHS0_Pos) 
+#define SUPC_VREGCTRL_AVREGEN_USBHS1_Msk             (_UINT32_(0x1) << SUPC_VREGCTRL_AVREGEN_USBHS1_Pos)
+#define SUPC_STATUS_ADDVREGRDY_USBHS0_Pos            _UINT32_(8)                                          
+#define SUPC_STATUS_ADDVREGRDY_USBHS0_Msk            (_UINT32_(0x1) << SUPC_STATUS_ADDVREGRDY_USBHS0_Pos) 
+#define SUPC_STATUS_ADDVREGRDY_USBHS1_Pos            _UINT32_(9)                                          
+#define SUPC_STATUS_ADDVREGRDY_USBHS1_Msk            (_UINT32_(0x1) << SUPC_STATUS_ADDVREGRDY_USBHS1_Pos)   
+
 /*********************************************
  * USB Driver object per USB Module instance 
  * present in the microcontroller.
@@ -211,7 +221,15 @@ void DRV_USBHS_Tasks
                 /* On PIC32MZ DA and EF devices, enable the global USB interrupt
                  * in the USBCRCON register. */
                 _DRV_USBHS_CLOCK_CONTROL_GLOBAL_USB_INT_ENABLE(usbID);
-
+                if (usbID == USBHS0_BASE_ADDRESS )
+                {
+                     /* Enable USBHS0 Voltage Regulator */ 
+                    SUPC_REGS->SUPC_VREGCTRL  |= SUPC_VREGCTRL_AVREGEN_USBHS0_Msk; 
+                    while ((SUPC_REGS->SUPC_STATUS & SUPC_STATUS_ADDVREGRDY_USBHS0_Msk) != SUPC_STATUS_ADDVREGRDY_USBHS0_Msk)
+                    {
+                        /* Do Nothing */
+                    }
+                }
                 /* Reset the PHY. This is a workaround for an errata */
                 PLIB_USBHS_SoftResetEnable(usbID);
 
