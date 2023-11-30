796d8a42eb826786a306094cea178b9f48d49ea8
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-30 11:36:38 +0530

    [Apps.] Regenerated demo for cdc_serial_emulator_sam_e54_xpro
    MH3-87977

--- apps/cdc_serial_emulator/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/cdc_serial_emulator/firmware/src/config/sam_e54_xpro/definitions.h
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
@@ -201,7 +201,7 @@ typedef struct
     SYS_MODULE_OBJ  usbDevObject0;
 
     SYS_MODULE_OBJ  drvUsart0;
-	SYS_MODULE_OBJ  drvUSBFSV1Object;
+    SYS_MODULE_OBJ  drvUSBFSV1Object;
 
 
 } SYSTEM_OBJECTS;
