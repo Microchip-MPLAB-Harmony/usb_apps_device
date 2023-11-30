3466cab0b23b1885f998b5a1373b17aa40647841
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-30 12:21:42 +0530

    [Apps.] Regenerated demo for hid_keyboard_sam_e70_xult_freertos
    MH3-88024

--- apps/hid_keyboard/firmware/src/config/sam_e70_xult_freertos/tasks.c
+++ apps/hid_keyboard/firmware/src/config/sam_e70_xult_freertos/tasks.c
@@ -60,23 +60,23 @@
 // Section: RTOS "Tasks" Routine
 // *****************************************************************************
 // *****************************************************************************
-void _DRV_USBHSV1_Tasks(  void *pvParameters  )
+static void F_DRV_USBHSV1_Tasks(  void *pvParameters  )
 {
-    while(1)
+    while(true)
     {
-				 /* USB HS Driver Task Routine */
+                 /* USB HS Driver Task Routine */
         DRV_USBHSV1_Tasks(sysObj.drvUSBHSV1Object);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 }
 
-void _USB_DEVICE_Tasks(  void *pvParameters  )
+static void F_USB_DEVICE_Tasks(  void *pvParameters  )
 {
-    while(1)
+    while(true)
     {
                 /* USB Device layer tasks routine */
         USB_DEVICE_Tasks(sysObj.usbDevObject0);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 }
 
@@ -116,8 +116,8 @@ void SYS_Tasks ( void )
     
 
     /* Maintain Middleware & Other Libraries */
-    	/* Create OS Thread for USB Driver Tasks. */
-    xTaskCreate( _DRV_USBHSV1_Tasks,
+        /* Create OS Thread for USB Driver Tasks. */
+    (void) xTaskCreate( F_DRV_USBHSV1_Tasks,
         "DRV_USBHSV1_TASKS",
         1024,
         (void*)NULL,
@@ -126,7 +126,7 @@ void SYS_Tasks ( void )
     );
 
     /* Create OS Thread for USB_DEVICE_Tasks. */
-    xTaskCreate( _USB_DEVICE_Tasks,
+    (void) xTaskCreate( F_USB_DEVICE_Tasks,
         "USB_DEVICE_TASKS",
         1024,
         (void*)NULL,
