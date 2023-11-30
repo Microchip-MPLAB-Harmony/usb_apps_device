4ef3d4255e02e2087bb1a2847edf51f343f24bed
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-30 11:49:00 +0530

    [Apps.] Regenerated demo for hid_mouse_sam_e54_xpro_freertos
    MH3-88030

--- apps/hid_mouse/firmware/src/config/sam_e54_xpro_freertos/tasks.c
+++ apps/hid_mouse/firmware/src/config/sam_e54_xpro_freertos/tasks.c
@@ -60,23 +60,23 @@
 // Section: RTOS "Tasks" Routine
 // *****************************************************************************
 // *****************************************************************************
-void _DRV_USBFSV1_Tasks(  void *pvParameters  )
+static void F_USB_DEVICE_Tasks(  void *pvParameters  )
 {
-    while(1)
-    {
-				 /* USB FS Driver Task Routine */
-        DRV_USBFSV1_Tasks(sysObj.drvUSBFSV1Object);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-}
-
-void _USB_DEVICE_Tasks(  void *pvParameters  )
-{
-    while(1)
+    while(true)
     {
                 /* USB Device layer tasks routine */
         USB_DEVICE_Tasks(sysObj.usbDevObject0);
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(10U / portTICK_PERIOD_MS);
+    }
+}
+
+static void F_DRV_USBFSV1_Tasks(  void *pvParameters  )
+{
+    while(true)
+    {
+                 /* USB FS Driver Task Routine */
+        DRV_USBFSV1_Tasks(sysObj.drvUSBFSV1Object);
+        vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 }
 
@@ -117,18 +117,18 @@ void SYS_Tasks ( void )
     
 
     /* Maintain Middleware & Other Libraries */
-    	/* Create OS Thread for USB Driver Tasks. */
-    xTaskCreate( _DRV_USBFSV1_Tasks,
-        "DRV_USBFSV1_TASKS",
+        /* Create OS Thread for USB_DEVICE_Tasks. */
+    (void) xTaskCreate( F_USB_DEVICE_Tasks,
+        "USB_DEVICE_TASKS",
         1024,
         (void*)NULL,
         1,
         (TaskHandle_t*)NULL
     );
 
-    /* Create OS Thread for USB_DEVICE_Tasks. */
-    xTaskCreate( _USB_DEVICE_Tasks,
-        "USB_DEVICE_TASKS",
+    /* Create OS Thread for USB Driver Tasks. */
+    (void) xTaskCreate( F_DRV_USBFSV1_Tasks,
+        "DRV_USBFSV1_TASKS",
         1024,
         (void*)NULL,
         1,
