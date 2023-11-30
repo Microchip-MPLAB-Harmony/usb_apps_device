078400b2d626de670b2d15c80d4d1f8237a651f1
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-30 08:45:24 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_pic32cm_le00_curiosity_pro
    MH3-87904

--- apps/cdc_com_port_dual/firmware/src/config/pic32cm_le00_curiosity_pro/configuration.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32cm_le00_curiosity_pro/configuration.h
@@ -94,36 +94,36 @@ extern "C" {
 // *****************************************************************************
 // *****************************************************************************
 /* Number of Endpoints used */
-#define DRV_USBFSV1_ENDPOINTS_NUMBER                        5
+#define DRV_USBFSV1_ENDPOINTS_NUMBER                        5U
 
 /* The USB Device Layer will not initialize the USB Driver */
 #define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
 
 /* Maximum device layer instances */
-#define USB_DEVICE_INSTANCES_NUMBER                         1
+#define USB_DEVICE_INSTANCES_NUMBER                         1U
 
 /* EP0 size in bytes */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64
+#define USB_DEVICE_EP0_BUFFER_SIZE                          64U
 
 
 /* Maximum instances of CDC function driver */
-#define USB_DEVICE_CDC_INSTANCES_NUMBER                     2
+#define USB_DEVICE_CDC_INSTANCES_NUMBER                     2U
 
 
 /* CDC Transfer Queue Size for both read and
    write. Applicable to all instances of the
    function driver */
-#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 6
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 6U
 
 /*** USB Driver Configuration ***/
 
 /* Maximum USB driver instances */
-#define DRV_USBFSV1_INSTANCES_NUMBER                        1
+#define DRV_USBFSV1_INSTANCES_NUMBER                        1U
 
 
 /* Enables Device Support */
 #define DRV_USBFSV1_DEVICE_SUPPORT                          true
-	
+    
 /* Disable Host Support */
 #define DRV_USBFSV1_HOST_SUPPORT                            false
 
