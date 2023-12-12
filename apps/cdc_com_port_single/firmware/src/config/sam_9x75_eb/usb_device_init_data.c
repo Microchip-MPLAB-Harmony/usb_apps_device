15d90f32997f848a4034dcd76202db2270430620
Shijas Mayan <shijas.mayan@microchip.com>
2023-12-12 14:54:53 +0530

    [Apps] Corrected the USB descriptors in the demo configuration "usb_apps_device/apps/cdc_com_port_single/firmware/sam_9x75_eb.X" MH3-88909

--- apps/cdc_com_port_single/firmware/src/config/sam_9x75_eb/usb_device_init_data.c
+++ apps/cdc_com_port_single/firmware/src/config/sam_9x75_eb/usb_device_init_data.c
@@ -104,7 +104,7 @@ static const USB_DEVICE_DESCRIPTOR usbDeviceDescriptor =
     0x0100,                                                 // Device release number in BCD format
     0x01,                                                   // Manufacturer string index
     0x02,                                                   // Product string index
-    0x03,                                                   // Device serial number string index
+    0x00,                                                   // Device serial number string index
     0x01                                                    // Number of possible configurations
 };
 
@@ -137,7 +137,7 @@ static const uint8_t highSpeedConfigurationDescriptor[]=
 
     0x09,                                               // Size of this descriptor in bytes
     (uint8_t)USB_DESCRIPTOR_CONFIGURATION,                       // Descriptor Type
-    USB_DEVICE_16bitTo8bitArrange(58),                  //(58 Bytes)Size of the Configuration descriptor
+    USB_DEVICE_16bitTo8bitArrange(67),                  //(67 Bytes)Size of the Configuration descriptor
     2,                                                  // Number of interfaces in this configuration
     0x01,                                               // Index value of this configuration
     0x00,                                               // Configuration string index
@@ -241,7 +241,7 @@ static const uint8_t fullSpeedConfigurationDescriptor[]=
 
     0x09,                                                   // Size of this descriptor in bytes
     (uint8_t)USB_DESCRIPTOR_CONFIGURATION,                           // Descriptor Type
-    USB_DEVICE_16bitTo8bitArrange(58),                      //(58 Bytes)Size of the Configuration descriptor
+    USB_DEVICE_16bitTo8bitArrange(67),                      //(67 Bytes)Size of the Configuration descriptor
     2,                                                      // Number of interfaces in this configuration
     0x01,                                                   // Index value of this configuration
     0x00,                                                   // Configuration string index
@@ -389,38 +389,15 @@ static sd002 =
     USB_DESCRIPTOR_STRING,
     {'S','i','m','p','l','e',' ','C','D','C',' ','D','e','v','i','c','e',' ','D','e','m','o'}
 };
-/******************************************************************************
- * Serial number string descriptor.  Note: This should be unique for each unit
- * built on the assembly line.  Plugging in two units simultaneously with the
- * same serial number into a single machine can cause problems.  Additionally,
- * not all hosts support all character values in the serial number string.  The
- * MSD Bulk Only Transport (BOT) specs v1.0 restrict the serial number to
- * consist only of ASCII characters "0" through "9" and capital letters "A"
- * through "F".
- ******************************************************************************/
-const struct
-{
-    uint8_t bLength;                                    // Size of this descriptor in bytes
-    uint8_t bDscType;                                   // STRING descriptor type
-    uint16_t string[12];                                // String
-}
-static serialNumberStringDescriptor =
-{
-    sizeof(serialNumberStringDescriptor),
-    USB_DESCRIPTOR_STRING,
-    {'1','2','3','4','5','6','7','8','9','0','1','2'}
-
-};
 
 /***************************************
  * Array of string descriptors
  ***************************************/
-static USB_DEVICE_STRING_DESCRIPTORS_TABLE stringDescriptors[4]=
+static USB_DEVICE_STRING_DESCRIPTORS_TABLE stringDescriptors[3]=
 {
     (const uint8_t *const)&sd000,
     (const uint8_t *const)&sd001,
     (const uint8_t *const)&sd002,
-    (const uint8_t *const)&serialNumberStringDescriptor,
 };
 
 /*******************************************
@@ -434,7 +411,7 @@ static const USB_DEVICE_MASTER_DESCRIPTOR usbMasterDescriptor =
     &usbDeviceDescriptor,                                      // High speed device descriptor
     1,                                                      // Total number of high speed configurations available
     highSpeedConfigDescSet,                                 // Pointer to array of high speed configurations descriptors
-    4,                                                      // Total number of string descriptors available.
+    3,                                                      // Total number of string descriptors available.
     stringDescriptors,                                      // Pointer to array of string descriptors.
     &deviceQualifierDescriptor1,                            // Pointer to full speed dev qualifier.
     &deviceQualifierDescriptor1                             // Pointer to high speed dev qualifier.
