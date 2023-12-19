2f02f21e8d96e86607dfe70b9c75a91674e9c9de
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-20 00:06:07 +0530

    [Apps.] Regenerated demo for vendor_sam_a5d27_som1_ek
    MH3-88087

--- apps/vendor/firmware/src/packs/ATSAMA5D27CD1G_DFP/instance/i2sc1.h
+++ apps/vendor/firmware/src/packs/ATSAMA5D27CD1G_DFP/instance/i2sc1.h
@@ -1,7 +1,7 @@
 /*
- * Instance header file for ATSAMA5D29
+ * Instance header file for ATSAMA5D29TA100
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,15 +20,15 @@
  *
  */
 
-/* file generated from device description version 2022-10-04T10:37:14Z */
+/* file generated from device description version 2022-11-18T13:40:01Z */
 #ifndef _SAMA5D_I2SC1_INSTANCE_
 #define _SAMA5D_I2SC1_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for I2SC1 peripheral ========== */
-#define I2SC1_INSTANCE_ID                        (55)       
 #define I2SC1_CLOCK_ID                           (55)       
-#define I2SC1_DMAC_ID_TX                         (33)       
 #define I2SC1_DMAC_ID_RX                         (34)       
+#define I2SC1_DMAC_ID_TX                         (33)       
+#define I2SC1_INSTANCE_ID                        (55)       
 
 #endif /* _SAMA5D_I2SC1_INSTANCE_ */
