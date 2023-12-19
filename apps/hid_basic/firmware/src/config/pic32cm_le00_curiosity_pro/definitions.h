489f7f1423b118247182688d82ac261b35a27bc6
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-19 22:43:02 +0530

    [Apps.] Regenerated demo for hid_basic_pic32cm_le00_curiosity_pro
    MH3-87983

--- apps/hid_basic/firmware/src/config/pic32cm_le00_curiosity_pro/definitions.h
+++ apps/hid_basic/firmware/src/config/pic32cm_le00_curiosity_pro/definitions.h
@@ -49,17 +49,17 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "usb/usb_device_hid.h"
 #include "usb/usb_hid.h"
-#include "peripheral/evsys/plib_evsys.h"
 #include "bsp/bsp.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/pm/plib_pm.h"
-#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "peripheral/pm/plib_pm.h"
+#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
@@ -194,9 +194,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  drvUSBFSV1Object;
-
     SYS_MODULE_OBJ  usbDevObject0;
+
+    SYS_MODULE_OBJ  drvUSBFSV1Object;
 
 
 } SYSTEM_OBJECTS;
