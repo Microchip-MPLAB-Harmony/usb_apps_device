184e87913a4bfb6116c9a529d0003c9c1d2434f3
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-20 00:12:25 +0530

    [Apps.] Regenerated demo for msd_sdcard_sam_a5d2_xult
    MH3-88063

--- apps/msd_sdcard/firmware/src/packs/ATSAMA5D27_DFP/instance/uart2.h
+++ apps/msd_sdcard/firmware/src/packs/ATSAMA5D27_DFP/instance/uart2.h
@@ -1,7 +1,7 @@
 /*
- * Instance header file for ATSAMA5D29
+ * Instance header file for ATSAMA5D29TA100
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,17 +20,17 @@
  *
  */
 
-/* file generated from device description version 2022-10-04T10:37:14Z */
+/* file generated from device description version 2022-11-18T13:40:01Z */
 #ifndef _SAMA5D_UART2_INSTANCE_
 #define _SAMA5D_UART2_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for UART2 peripheral ========== */
-#define UART2_INSTANCE_ID                        (26)       
-#define UART2_CLOCK_ID                           (26)       
-#define UART2_DMAC_ID_TX                         (39)       
-#define UART2_DMAC_ID_RX                         (40)       
-#define UART2_BRSRCCK_PERIPH_CLK                 (0)        /* MCK */
 #define UART2_BRSRCCK_GCLK                       (0)        /* GCLK */
+#define UART2_BRSRCCK_PERIPH_CLK                 (0)        /* MCK */
+#define UART2_CLOCK_ID                           (26)       
+#define UART2_DMAC_ID_RX                         (40)       
+#define UART2_DMAC_ID_TX                         (39)       
+#define UART2_INSTANCE_ID                        (26)       
 
 #endif /* _SAMA5D_UART2_INSTANCE_ */
