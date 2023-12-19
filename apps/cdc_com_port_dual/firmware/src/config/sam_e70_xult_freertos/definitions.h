7e913d497eeb9de4b2ab3b7f341f5fa5663576e2
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-19 23:31:42 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_sam_e70_xult_freertos
    MH3-87917

--- apps/cdc_com_port_dual/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/cdc_com_port_dual/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -50,8 +50,8 @@
 #include <stdbool.h>
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
-#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "bsp/bsp.h"
+#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
