2c28a6008373c168a534b6668e76932f03c2331f
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-19 22:56:04 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_pic32mx470_curiosity
    MH3-87907

--- apps/cdc_com_port_dual/firmware/src/config/pic32mx470_curiosity/configuration.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32mx470_curiosity/configuration.h
@@ -102,19 +102,6 @@ extern "C" {
    function driver */
 #define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 6U
 
-/* Number of Endpoints used */
-#define DRV_USBFS_ENDPOINTS_NUMBER                        6U
-
-/* The USB Device Layer will not initialize the USB Driver */
-#define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
-
-/* Maximum device layer instances */
-#define USB_DEVICE_INSTANCES_NUMBER                         1U
-
-/* EP0 size in bytes */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64U
-
-
 /*** USB Driver Configuration ***/
 
 /* Maximum USB driver instances */
@@ -135,6 +122,19 @@ extern "C" {
 
 /* Alignment for buffers that are submitted to USB Driver*/ 
 #define USB_ALIGN  CACHE_ALIGN
+
+/* Number of Endpoints used */
+#define DRV_USBFS_ENDPOINTS_NUMBER                        6U
+
+/* The USB Device Layer will not initialize the USB Driver */
+#define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
+
+/* Maximum device layer instances */
+#define USB_DEVICE_INSTANCES_NUMBER                         1U
+
+/* EP0 size in bytes */
+#define USB_DEVICE_EP0_BUFFER_SIZE                          64U
+
 
 
 
