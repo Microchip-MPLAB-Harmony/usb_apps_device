47e7697b17ccdcda3010da1e20100a4884680b77
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-01-03 09:46:12 +0530

    [Apps.]Updated release notes[MH3-89283]

--- docs/docs_html/oxygen-webhelp/app/config.js
+++ docs/docs_html/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20231212115924';
+                args = '20240102125755';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
