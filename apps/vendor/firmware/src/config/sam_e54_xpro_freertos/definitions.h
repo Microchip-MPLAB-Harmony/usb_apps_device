e5a7ee6a60c92247c721d55c63a36fc18b4334ff
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 21:09:14 +0530

    [Apps.] Regenerated demo for vendor_sam_e54_xpro_freertos
    MH3-93882

--- apps/vendor/firmware/src/config/sam_e54_xpro_freertos/definitions.h
+++ apps/vendor/firmware/src/config/sam_e54_xpro_freertos/definitions.h
@@ -55,9 +55,9 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
+#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
-#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "system/int/sys_int.h"
@@ -195,9 +195,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  usbDevObject0;
-
     SYS_MODULE_OBJ  drvUSBFSV1Object;
+
+    SYS_MODULE_OBJ  usbDevObject0;
 
 
 } SYSTEM_OBJECTS;
