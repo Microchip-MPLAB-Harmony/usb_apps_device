aa6dc08460aa9d9f92bbcec6fd693574b152d420
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 21:18:45 +0530

    [Apps.] Regenerated demo for cdc_com_port_single_pic32mk_gp_db
    MH3-93692

--- apps/cdc_com_port_single/firmware/src/config/pic32mk_gp_db/driver/usb/usbfs/src/templates/usb_LastEndpoint_Default.h
+++ apps/cdc_com_port_single/firmware/src/config/pic32mk_gp_db/driver/usb/usbfs/src/templates/usb_LastEndpoint_Default.h
@@ -48,7 +48,6 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 
 #include "driver/usb/usbfs/src/templates/usbfs_registers.h"
 
-/* MISRA C-2012 Rule 11.7 deviated:2 Deviation record ID -  H3_MISRAC_2012_R_11_7_DR_1 */
 //******************************************************************************
 /* Function :  USB_LastTransactionEndPtGet_Default
 
@@ -81,7 +80,6 @@ PLIB_TEMPLATE bool USB_ExistsLastEndpoint_Default( USB_MODULE_ID index )
     return true;
 }
 
-/* MISRAC 2012 deviation block end */
 
 #endif /*USB_LASTENDPOINT_DEFAULT_H*/
 
