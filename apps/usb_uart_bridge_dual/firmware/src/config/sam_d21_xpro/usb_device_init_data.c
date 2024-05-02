7c18b18604ce2c54e644fed485bfb2ec955bdb44
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 21:03:36 +0530

    [Apps.] Regenerated demo for usb_uart_bridge_dual_sam_d21_xpro
    MH3-93857

--- apps/usb_uart_bridge_dual/firmware/src/config/sam_d21_xpro/usb_device_init_data.c
+++ apps/usb_uart_bridge_dual/firmware/src/config/sam_d21_xpro/usb_device_init_data.c
@@ -49,7 +49,7 @@
 /**************************************************
  * USB Device Function Driver Init Data
  **************************************************/
-/* MISRA C-2012 Rule 10.3 deviated:4 Deviation record ID -  H3_MISRAC_2012_R_10_3_DR_1 */
+/* MISRA C-2012 Rule 10.3 deviated:4 Deviation record ID -  H3_USB_MISRAC_2012_R_10_3_DR_1 */
 static const USB_DEVICE_CDC_INIT cdcInit0 =
 {
     .queueSizeRead = 1,
@@ -59,7 +59,7 @@ static const USB_DEVICE_CDC_INIT cdcInit0 =
 /* MISRAC 2012 deviation block end */   
 
 
-/* MISRA C-2012 Rule 10.3 deviated:4 Deviation record ID -  H3_MISRAC_2012_R_10_3_DR_1 */
+/* MISRA C-2012 Rule 10.3 deviated:4 Deviation record ID -  H3_USB_MISRAC_2012_R_10_3_DR_1 */
 static const USB_DEVICE_CDC_INIT cdcInit1 =
 {
     .queueSizeRead = 1,
@@ -75,7 +75,7 @@ static const USB_DEVICE_CDC_INIT cdcInit1 =
  * Table
  **************************************************/
 /* MISRA C-2012 Rule 10.3 deviated:2, 11.8 deviated:6 deviated below. Deviation record ID -  
-   H3_MISRAC_2012_R_10_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+   H3_USB_MISRAC_2012_R_10_3_DR_1 & H3_USB_MISRAC_2012_R_11_8_DR_1*/
 static const USB_DEVICE_FUNCTION_REGISTRATION_TABLE funcRegistrationTable[2] =
 {
         /* CDC Function 0 */
@@ -131,7 +131,7 @@ static const USB_DEVICE_DESCRIPTOR usbDeviceDescriptor =
 /*******************************************
  *  USB Full Speed Configuration Descriptor
  *******************************************/
- /* MISRA C-2012 Rule 10.3 deviated:25 Deviation record ID -  H3_MISRAC_2012_R_10_3_DR_1 */
+ /* MISRA C-2012 Rule 10.3 deviated:25 Deviation record ID -  H3_USB_MISRAC_2012_R_10_3_DR_1 */
 static const uint8_t fullSpeedConfigurationDescriptor[]=
 {
     /* Configuration Descriptor */
@@ -143,7 +143,7 @@ static const uint8_t fullSpeedConfigurationDescriptor[]=
     0x01,                                                   // Index value of this configuration
     0x00,                                                   // Configuration string index
     USB_ATTRIBUTE_DEFAULT | USB_ATTRIBUTE_SELF_POWERED, // Attributes
-    50,                                                 // Maximum power consumption (mA) /2
+    50,                                                 // Maximum Power: 100mA
 
     /* Descriptor for Function - CDC     */
     /* Interface Association Descriptor: CDC Function*/
@@ -352,7 +352,7 @@ static sd000 =
 /*******************************************
  *  Manufacturer string descriptor
  *******************************************/
-/* MISRA C-2012 Rule 10.3 deviated:43 Deviation record ID -  H3_MISRAC_2012_R_10_3_DR_1 */
+/* MISRA C-2012 Rule 10.3 deviated:43 Deviation record ID -  H3_USB_MISRAC_2012_R_10_3_DR_1 */
 
 const struct
 {
