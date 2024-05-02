eff7493269ee8f27ee535defadcfb772a723204f
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 21:07:08 +0530

    [Apps.] Regenerated demo for hid_basic_remote_wakeup_sam_e54_xpro
    MH3-93788

--- apps/hid_basic_remote_wakeup/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/hid_basic_remote_wakeup/firmware/src/config/sam_e54_xpro/definitions.h
@@ -48,17 +48,17 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/pm/plib_pm.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/eic/plib_eic.h"
 #include "system/time/sys_time.h"
 #include "bsp/bsp.h"
@@ -204,9 +204,9 @@ typedef struct
 {
     SYS_MODULE_OBJ  usbDevObject0;
 
+    SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  sysDebug;
 
-    SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  drvUSBFSV1Object;
 
     SYS_MODULE_OBJ  sysConsole0;
