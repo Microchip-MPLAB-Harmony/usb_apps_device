83d0682876fb4e3cb624e9fd7543720153b9b306
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 20:39:34 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_pic32mm_usb_curiosity
    MH3-93670

--- apps/cdc_com_port_dual/firmware/src/config/pic32mm_usb_curiosity/driver/usb/usbfs/src/templates/usb_OTG_VbusCharge_Default.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32mm_usb_curiosity/driver/usb/usbfs/src/templates/usb_OTG_VbusCharge_Default.h
@@ -49,7 +49,6 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 
 #include "driver/usb/usbfs/src/templates/usbfs_registers.h"
 
-/* MISRA C-2012 Rule 11.7 deviated:2 Deviation record ID -  H3_MISRAC_2012_R_11_7_DR_1 */
 
 //******************************************************************************
 /* Function :  USB_OTG_VBusChargeEnable_Default
@@ -100,7 +99,6 @@ PLIB_TEMPLATE bool USB_ExistsOTG_VbusCharge_Default( USB_MODULE_ID index )
     return true;
 }
 
-/* MISRAC 2012 deviation block end */
 
 #endif /*USB_OTG_VBUSCHARGE_DEFAULT_H*/
 
