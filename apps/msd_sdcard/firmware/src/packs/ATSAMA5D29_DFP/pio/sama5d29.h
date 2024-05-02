d08f050616c4eef176b5cb6154b24a750d8584ed
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 20:57:41 +0530

    [Apps.] Regenerated demo for msd_sdcard_sam_a5d29_curiosity
    MH3-93846

--- apps/msd_sdcard/firmware/src/packs/ATSAMA5D29_DFP/pio/sama5d29.h
+++ apps/msd_sdcard/firmware/src/packs/ATSAMA5D29_DFP/pio/sama5d29.h
@@ -1,7 +1,7 @@
 /*
  * Peripheral I/O description for SAMA5D29
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2022-10-04T10:37:14Z */
+/* file generated from device description version 2022-10-28T12:33:58Z */
 #ifndef _SAMA5D29_GPIO_H_
 #define _SAMA5D29_GPIO_H_
 
