2fbe601b595919e539fe1494c99bef3e5fcab406
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 11:46:46 +0530

    [Apps.] Regenerated demo for printer_basic_sam_e54_xpro
    MH3-93856

--- apps/printer_basic/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/printer_basic/firmware/src/config/sam_e54_xpro/definitions.h
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/sercom/usart/plib_sercom2_usart.h"
 #include "usb/usb_device_printer.h"
 #include "usb/usb_printer.h"
 #include "peripheral/evsys/plib_evsys.h"
