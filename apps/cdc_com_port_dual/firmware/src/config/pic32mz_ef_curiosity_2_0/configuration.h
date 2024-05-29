4166b969cd3caf57a9de5e7a9c1b54f44b1c2210
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 18:36:03 +0530

    [Apps.] Regenerated demo for cdc_com_port_dual_pic32mz_ef_curiosity_2_0
    MH3-93672

--- apps/cdc_com_port_dual/firmware/src/config/pic32mz_ef_curiosity_2_0/configuration.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32mz_ef_curiosity_2_0/configuration.h
@@ -93,6 +93,15 @@ extern "C" {
 // Section: Middleware & Other Library Configuration
 // *****************************************************************************
 // *****************************************************************************
+/* Maximum instances of CDC function driver */
+#define USB_DEVICE_CDC_INSTANCES_NUMBER                     2U
+
+
+/* CDC Transfer Queue Size for both read and
+   write. Applicable to all instances of the
+   function driver */
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 6U
+
 /*** USB Driver Configuration ***/
 
 /* Maximum USB driver instances */
@@ -115,15 +124,6 @@ extern "C" {
 /* Alignment for buffers that are submitted to USB Driver*/ 
 #define USB_ALIGN  CACHE_ALIGN  __ALIGNED(16)
 
-
-/* Maximum instances of CDC function driver */
-#define USB_DEVICE_CDC_INSTANCES_NUMBER                     2U
-
-
-/* CDC Transfer Queue Size for both read and
-   write. Applicable to all instances of the
-   function driver */
-#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 6U
 
 /* Number of Endpoints used */
 #define DRV_USBHS_ENDPOINTS_NUMBER                        6U
