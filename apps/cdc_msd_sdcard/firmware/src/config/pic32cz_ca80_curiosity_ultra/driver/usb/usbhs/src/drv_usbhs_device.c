c23f2bee3307fbc5738d522e72d5e42bd1a58183
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 18:26:38 +0530

    [Apps.] Regenerated demo for cdc_msd_sdcard_pic32cz_ca80_curiosity_ultra
    MH3-93733

--- apps/cdc_msd_sdcard/firmware/src/config/pic32cz_ca80_curiosity_ultra/driver/usb/usbhs/src/drv_usbhs_device.c
+++ apps/cdc_msd_sdcard/firmware/src/config/pic32cz_ca80_curiosity_ultra/driver/usb/usbhs/src/drv_usbhs_device.c
@@ -348,7 +348,7 @@ uint16_t F_DRV_USBHS_ProcessIRPFIFO
     DRV_USBHS_DEVICE_ENDPOINT_OBJ * endpointObj,
     uint8_t endpoint,
     uint8_t direction,
-    USB_DEVICE_IRP_LOCAL * irp,
+    DRV_USBHS_DEVICE_IRP_LOCAL * irp,
     bool * pisDMAUsed,
     bool tryDma
 )
@@ -1646,7 +1646,7 @@ USB_ERROR DRV_USBHS_DEVICE_IRPSubmit
     int32_t direction;
     USBHS_MODULE_ID usbID = USBHS_NUMBER_OF_MODULES;
     DRV_USBHS_OBJ * hDriver =  NULL;
-    USB_DEVICE_IRP_LOCAL * irp_t = (USB_DEVICE_IRP_LOCAL *)irp;
+    DRV_USBHS_DEVICE_IRP_LOCAL * irp_t = (DRV_USBHS_DEVICE_IRP_LOCAL *)irp;
     DRV_USBHS_DEVICE_ENDPOINT_OBJ * endpointObj = NULL;
     bool interruptWasEnabled = false;
     bool interruptWasEnabledUSBDMA = false;
@@ -2007,7 +2007,7 @@ USB_ERROR DRV_USBHS_DEVICE_IRPSubmit
                         {
                             /* This means we should surf the linked list to get to the last
                              * entry . */
-                            USB_DEVICE_IRP_LOCAL * iterator;
+                            DRV_USBHS_DEVICE_IRP_LOCAL * iterator;
                             iterator = endpointObj->irpQueue;
                             while(iterator->next != NULL)
                             {
@@ -2081,7 +2081,7 @@ void F_DRV_USBHS_DEVICE_IRPQueueFlush
 
 )
 {
-    USB_DEVICE_IRP_LOCAL * iterator;
+    DRV_USBHS_DEVICE_IRP_LOCAL * iterator;
     
     /* Check if any IRPs are assigned on this endpoint and 
      * abort them */
@@ -2280,7 +2280,7 @@ USB_ERROR DRV_USBHS_DEVICE_IRPCancel
 {
     USB_ERROR returnValue = USB_ERROR_PARAMETER_INVALID;
     DRV_USBHS_OBJ * hDriver = NULL;
-    USB_DEVICE_IRP_LOCAL * irpToCancel = (USB_DEVICE_IRP_LOCAL *)irp;
+    DRV_USBHS_DEVICE_IRP_LOCAL * irpToCancel = (DRV_USBHS_DEVICE_IRP_LOCAL *)irp;
     bool interruptWasEnabled = false;
     bool interruptWasEnabledUSBDMA = false;
     bool mutexLock = false;
@@ -2771,7 +2771,7 @@ void F_DRV_USBHS_DEVICE_Tasks_ISR
     DRV_USBHS_DEVICE_ENDPOINT_OBJ * endpointObjReceive = NULL;
     DRV_USBHS_DEVICE_ENDPOINT_OBJ * endpointObjTransmit = NULL;
     USBHS_MODULE_ID usbID = USBHS_NUMBER_OF_MODULES;
-    USB_DEVICE_IRP_LOCAL * irp = NULL;
+    DRV_USBHS_DEVICE_IRP_LOCAL * irp = NULL;
     uint32_t endpoint0DataStageDirection = 0;
     DRV_USBHS_CLIENT_OBJ * deviceModeClient = NULL;
     uint32_t temp_32;
@@ -3732,7 +3732,7 @@ void F_DRV_USBHS_DEVICE_Tasks_ISR_USBDMA
     bool endpointDir = false;
     DRV_USBHS_DEVICE_ENDPOINT_OBJ * endpointObjReceive = NULL;
     DRV_USBHS_DEVICE_ENDPOINT_OBJ * endpointObjTransmit = NULL;
-    USB_DEVICE_IRP_LOCAL * irp = NULL;
+    DRV_USBHS_DEVICE_IRP_LOCAL * irp = NULL;
 
     usbID = hDriver->usbDrvCommonObj.usbID;
 
