7890107043a1a1978c01af3cdb78ba56b9f8393b
Arunprasath P <arunprasath.p@microchip.com>
2024-09-20 23:21:55 +0530

    [Apps.] Added the demo application 'hid_basic' for WBZ653 Curiosity Board - WBZ653. MH3-90066

---
+++ apps/hid_basic/firmware/src/packs/WBZ653_DFP/instance/freqm.h
@@ -0,0 +1,31 @@
+/*
+ * Instance header file for WBZ653
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2024-09-25T20:48:23Z */
+#ifndef _WBZ65_FREQM_INSTANCE_
+#define _WBZ65_FREQM_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for FREQM peripheral ========== */
+#define FREQM_GCLK_ID_MSR                        (1)        
+#define FREQM_GCLK_ID_REF                        (2)        
+#define FREQM_INSTANCE_ID                        (1)        /* Instance index for FREQM */
+#define FREQM_PAC_ID                             (1)        /* Index for FREQM registers write protection */
+
+#endif /* _WBZ65_FREQM_INSTANCE_ */
---
+++ apps/hid_basic/firmware/src/packs/WBZ653_DFP/instance/tc3.h
@@ -0,0 +1,34 @@
+/*
+ * Instance header file for WBZ653
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2024-09-25T20:48:23Z */
+#ifndef _WBZ65_TC3_INSTANCE_
+#define _WBZ65_TC3_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for TC3 peripheral ========== */
+#define TC3_DMAC_ID_MC0                          (45)       
+#define TC3_DMAC_ID_MC1                          (46)       
+#define TC3_DMAC_ID_OVF                          (44)       
+#define TC3_GCLK_ID                              (26)       
+#define TC3_INSTANCE_ID                          (8)        /* Instance index for TC3 */
+#define TC3_MASTER_SLAVE_MODE                    (2)        
+#define TC3_PAC_ID                               (8)        /* Index for TC3 registers write protection */
+
+#endif /* _WBZ65_TC3_INSTANCE_ */
