09bacaaf3fd70473d16997b81133478b23996730
Arunprasath P <arunprasath.p@microchip.com>
2024-09-25 15:31:53 +0530

    [Apps.] Added the demo application 'cdc_com_port_single' for WBZ653 Curiosity Board - WBZ653. MH3-90064

---
+++ apps/cdc_com_port_single/firmware/src/packs/WBZ653_DFP/instance/tc6.h
@@ -0,0 +1,34 @@
+/*
+ * Instance header file for WBZ653
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2024-09-25T20:48:23Z */
+#ifndef _WBZ65_TC6_INSTANCE_
+#define _WBZ65_TC6_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for TC6 peripheral ========== */
+#define TC6_DMAC_ID_MC0                          (54)       
+#define TC6_DMAC_ID_MC1                          (55)       
+#define TC6_DMAC_ID_OVF                          (53)       
+#define TC6_GCLK_ID                              (28)       
+#define TC6_INSTANCE_ID                          (11)       /* Instance index for TC6 */
+#define TC6_MASTER_SLAVE_MODE                    (1)        
+#define TC6_PAC_ID                               (11)       /* Index for TC6 registers write protection */
+
+#endif /* _WBZ65_TC6_INSTANCE_ */
---
+++ apps/cdc_com_port_single/firmware/src/packs/WBZ653_DFP/instance/tc7.h
@@ -0,0 +1,34 @@
+/*
+ * Instance header file for WBZ653
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2024-09-25T20:48:23Z */
+#ifndef _WBZ65_TC7_INSTANCE_
+#define _WBZ65_TC7_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for TC7 peripheral ========== */
+#define TC7_DMAC_ID_MC0                          (57)       
+#define TC7_DMAC_ID_MC1                          (58)       
+#define TC7_DMAC_ID_OVF                          (56)       
+#define TC7_GCLK_ID                              (28)       
+#define TC7_INSTANCE_ID                          (12)       /* Instance index for TC7 */
+#define TC7_MASTER_SLAVE_MODE                    (2)        
+#define TC7_PAC_ID                               (12)       /* Index for TC7 registers write protection */
+
+#endif /* _WBZ65_TC7_INSTANCE_ */
