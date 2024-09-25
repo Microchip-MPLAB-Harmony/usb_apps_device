b278a7307660d10957f39015a41370024d50420e
Arunprasath P <arunprasath.p@microchip.com>
2024-09-25 14:00:24 +0530

    [Apps.] Added the demo application 'cdc_msd_basic' for WBZ653 Curiosity Board - WBZ653. MH3-90065

---
+++ apps/cdc_msd_basic/firmware/src/packs/WBZ653_DFP/instance/tc9.h
@@ -0,0 +1,34 @@
+/*
+ * Instance header file for WBZ653
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2024-09-25T20:48:23Z */
+#ifndef _WBZ65_TC9_INSTANCE_
+#define _WBZ65_TC9_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for TC9 peripheral ========== */
+#define TC9_DMAC_ID_MC0                          (63)       
+#define TC9_DMAC_ID_MC1                          (64)       
+#define TC9_DMAC_ID_OVF                          (62)       
+#define TC9_GCLK_ID                              (29)       
+#define TC9_INSTANCE_ID                          (101)      /* Instance index for TC9 */
+#define TC9_MASTER_SLAVE_MODE                    (2)        
+#define TC9_PAC_ID                               (101)      /* Index for TC9 registers write protection */
+
+#endif /* _WBZ65_TC9_INSTANCE_ */
