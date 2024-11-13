c743a344f81737dbedc74be3204a74c859dc0102
Arunprasath P <arunprasath.p@microchip.com>
2024-11-13 08:49:49 +0530

    Add the demo application 'hid_joystick' for SAM9X75 Curiosity Board. MH3-105157

---
+++ apps/hid_joystick/firmware/src/config/sam_9x75_curiosity/user.h
@@ -0,0 +1,54 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+#ifndef USER_H
+#define USER_H
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+#define APP_USB_SWITCH_DEBOUNCE_COUNT_FS                    150
+#define APP_USB_SWITCH_DEBOUNCE_COUNT_HS                    150
+    
+#define SWITCH_STATE_PRESSED  0
+#define SWITCH_STATE_RELEASED 1
+    
+#define LED_On()       LED_Set()
+#define LED_Off()      LED_Clear()
+
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
