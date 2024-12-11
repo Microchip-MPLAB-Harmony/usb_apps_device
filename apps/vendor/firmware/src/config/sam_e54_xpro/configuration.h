938d034bd36bd013198fc0edf16d41d7b34d7603
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 17:45:30 +0530

    Regenerated demo for vendor_sam_e54_xpro
    MH3-107844

--- apps/vendor/firmware/src/config/sam_e54_xpro/configuration.h
+++ apps/vendor/firmware/src/config/sam_e54_xpro/configuration.h
@@ -93,6 +93,19 @@ extern "C" {
 // Section: Middleware & Other Library Configuration
 // *****************************************************************************
 // *****************************************************************************
+/* Number of Endpoints used */
+#define DRV_USBFSV1_ENDPOINTS_NUMBER                        2U
+
+/* The USB Device Layer will not initialize the USB Driver */
+#define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
+
+/* Maximum device layer instances */
+#define USB_DEVICE_INSTANCES_NUMBER                         1U
+
+/* EP0 size in bytes */
+#define USB_DEVICE_EP0_BUFFER_SIZE                          64U
+
+
 /*** USB Driver Configuration ***/
 
 /* Maximum USB driver instances */
@@ -110,19 +123,6 @@ extern "C" {
 
 /* Alignment for buffers that are submitted to USB Driver*/ 
 #define USB_ALIGN  __ALIGNED(CACHE_LINE_SIZE)
-
-/* Number of Endpoints used */
-#define DRV_USBFSV1_ENDPOINTS_NUMBER                        2U
-
-/* The USB Device Layer will not initialize the USB Driver */
-#define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
-
-/* Maximum device layer instances */
-#define USB_DEVICE_INSTANCES_NUMBER                         1U
-
-/* EP0 size in bytes */
-#define USB_DEVICE_EP0_BUFFER_SIZE                          64U
-
 
 /* Endpoint Transfer Queue Size for both read and
    write. Applicable to all instances of the
