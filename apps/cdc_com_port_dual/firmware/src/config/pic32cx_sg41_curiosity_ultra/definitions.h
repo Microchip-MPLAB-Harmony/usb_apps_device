e697d76001bf76cbd41ad206d50ebbac023087bb
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 16:40:33 +0530

    Regenerated demo for cdc_com_port_dual_pic32cx_sg41_curiosity_ultra
    MH3-107348

--- apps/cdc_com_port_dual/firmware/src/config/pic32cx_sg41_curiosity_ultra/definitions.h
+++ apps/cdc_com_port_dual/firmware/src/config/pic32cx_sg41_curiosity_ultra/definitions.h
@@ -57,9 +57,9 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
@@ -77,13 +77,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CX1025SG41128"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "PIC32CX"
-#define DEVICE_SERIES		 "PIC32CXSG41"
+#define DEVICE_NAME          "PIC32CX1025SG41128"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "PIC32CX"
+#define DEVICE_SERIES        "PIC32CXSG41"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 119999488
+#define CPU_CLOCK_FREQUENCY 119999488U
 
 // *****************************************************************************
 // *****************************************************************************
@@ -195,9 +195,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  drvUSBFSV1Object;
-
     SYS_MODULE_OBJ  usbDevObject0;
+
+    SYS_MODULE_OBJ  drvUSBFSV1Object;
 
 
 } SYSTEM_OBJECTS;
