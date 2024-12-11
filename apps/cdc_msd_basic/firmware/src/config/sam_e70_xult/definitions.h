d66b0ba8fb70cae50de4253b468b87a299225014
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 17:53:23 +0530

    Regenerated demo for cdc_msd_basic_sam_e70_xult
    MH3-107492

--- apps/cdc_msd_basic/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/cdc_msd_basic/firmware/src/config/sam_e70_xult/definitions.h
@@ -54,14 +54,14 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/mpu/plib_mpu.h"
+#include "driver/memory/drv_memory.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
-#include "driver/memory/drv_memory.h"
 #include "peripheral/efc/plib_efc.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
-#include "bsp/bsp.h"
 #include "driver/usb/usbhsv1/drv_usbhsv1.h"
+#include "bsp/bsp.h"
 #include "driver/memory/drv_memory_efc.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
@@ -80,13 +80,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAME70Q21B"
-#define DEVICE_ARCH			 "CORTEX-M7"
-#define DEVICE_FAMILY		 "SAME"
-#define DEVICE_SERIES		 "SAME70"
+#define DEVICE_NAME          "ATSAME70Q21B"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "SAME"
+#define DEVICE_SERIES        "SAME70"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
