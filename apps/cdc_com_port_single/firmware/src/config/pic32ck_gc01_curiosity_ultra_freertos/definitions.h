9c3447278afd463573d5b3b319d9d40d3256784d
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 18:02:57 +0530

    Regenerated demo for cdc_com_port_single_pic32ck_gc01_curiosity_ultra_freertos
    MH3-107392

--- apps/cdc_com_port_single/firmware/src/config/pic32ck_gc01_curiosity_ultra_freertos/definitions.h
+++ apps/cdc_com_port_single/firmware/src/config/pic32ck_gc01_curiosity_ultra_freertos/definitions.h
@@ -79,13 +79,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CK2051GC01144"
-#define DEVICE_ARCH			 "CORTEX-M33"
-#define DEVICE_FAMILY		 "PIC32CK"
-#define DEVICE_SERIES		 "PIC32CKGC01"
+#define DEVICE_NAME          "PIC32CK2051GC01144"
+#define DEVICE_ARCH          "CORTEX-M33"
+#define DEVICE_FAMILY        "PIC32CK"
+#define DEVICE_SERIES        "PIC32CKGC01"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
