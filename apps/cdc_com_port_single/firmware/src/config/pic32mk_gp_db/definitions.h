7248cf9cefe815bad03275058eaedd08d9b934c8
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 18:05:40 +0530

    Regenerated demo for cdc_com_port_single_pic32mk_gp_db
    MH3-107408

--- apps/cdc_com_port_single/firmware/src/config/pic32mk_gp_db/definitions.h
+++ apps/cdc_com_port_single/firmware/src/config/pic32mk_gp_db/definitions.h
@@ -73,13 +73,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MK1024GPE100"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MK1402"
-#define DEVICE_SERIES		 "PIC32MK"
+#define DEVICE_NAME          "PIC32MK1024GPE100"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MK1402"
+#define DEVICE_SERIES        "PIC32MK"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
