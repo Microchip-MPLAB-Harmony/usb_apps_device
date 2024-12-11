7abccf9a095a89fdc1083606e79ba8a2883904bb
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 16:59:49 +0530

    Regenerated demo for printer_basic_pic32cz_ca80_curiosity_ultra
    MH3-107774

--- apps/printer_basic/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
+++ apps/printer_basic/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
@@ -49,9 +49,9 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/sercom/usart/plib_sercom1_usart.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "usb/usb_device_printer.h"
 #include "usb/usb_printer.h"
-#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
@@ -77,13 +77,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CZ8110CA80208"
-#define DEVICE_ARCH			 "CORTEX-M7"
-#define DEVICE_FAMILY		 "PIC32C"
-#define DEVICE_SERIES		 "PIC32CZCA80"
+#define DEVICE_NAME          "PIC32CZ8110CA80208"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "PIC32C"
+#define DEVICE_SERIES        "PIC32CZCA80"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
--- apps/printer_basic/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/sercom/usart/plib_sercom1_usart.c
+++ apps/printer_basic/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/sercom/usart/plib_sercom1_usart.c
@@ -47,21 +47,18 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "plib_sercom1_usart.h"
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data
 // *****************************************************************************
 // *****************************************************************************
-
-
 /* SERCOM1 USART baud value for 115200 Hz baud rate */
 #define SERCOM1_USART_INT_BAUD_VALUE            (64730UL)
 
 volatile static SERCOM_USART_OBJECT sercom1USARTObj;
+
 
 // *****************************************************************************
 // *****************************************************************************
@@ -102,6 +99,7 @@ void SERCOM1_USART_Initialize( void )
      * Configures Sampling rate
      * Configures IBON
      */
+
     SERCOM1_REGS->USART_INT.SERCOM_CTRLA = SERCOM_USART_INT_CTRLA_MODE_USART_INT_CLK | SERCOM_USART_INT_CTRLA_RXPO(0x3UL) | SERCOM_USART_INT_CTRLA_TXPO(0x0UL) | SERCOM_USART_INT_CTRLA_DORD_Msk | SERCOM_USART_INT_CTRLA_IBON_Msk | SERCOM_USART_INT_CTRLA_FORM(0x0UL) | SERCOM_USART_INT_CTRLA_SAMPR(0UL) ;
 
     /* Configure Baud Rate */
@@ -146,6 +144,10 @@ void SERCOM1_USART_Initialize( void )
     sercom1USARTObj.errorStatus = USART_ERROR_NONE;
 }
 
+
+
+
+
 uint32_t SERCOM1_USART_FrequencyGet( void )
 {
     return 150000000UL;
@@ -156,6 +158,7 @@ bool SERCOM1_USART_SerialSetup( USART_SERIAL_SETUP * serialSetup, uint32_t clkFr
     bool setupStatus       = false;
     uint32_t baudValue     = 0U;
     uint32_t sampleRate    = 0U;
+    uint32_t sampleCount   = 0U;
 
     bool transferProgress = sercom1USARTObj.txBusyStatus;
     transferProgress = sercom1USARTObj.rxBusyStatus || transferProgress;
@@ -174,24 +177,24 @@ bool SERCOM1_USART_SerialSetup( USART_SERIAL_SETUP * serialSetup, uint32_t clkFr
 
         if(clkFrequency >= (16U * serialSetup->baudRate))
         {
-            baudValue = 65536U - (uint32_t)(((uint64_t)65536U * 16U * serialSetup->baudRate) / clkFrequency);
             sampleRate = 0U;
+            sampleCount = 16U;
         }
         else if(clkFrequency >= (8U * serialSetup->baudRate))
         {
-            baudValue = 65536U - (uint32_t)(((uint64_t)65536U * 8U * serialSetup->baudRate) / clkFrequency);
             sampleRate = 2U;
+            sampleCount = 8U;
         }
         else if(clkFrequency >= (3U * serialSetup->baudRate))
         {
-            baudValue = 65536U - (uint32_t)(((uint64_t)65536U * 3U * serialSetup->baudRate) / clkFrequency);
             sampleRate = 4U;
+            sampleCount = 3U;
         }
         else
         {
             /* Do nothing */
         }
-
+        baudValue = 65536U - (uint32_t)(((uint64_t)65536U * sampleCount * serialSetup->baudRate) / clkFrequency);
         /* Disable the USART before configurations */
         SERCOM1_REGS->USART_INT.SERCOM_CTRLA &= ~SERCOM_USART_INT_CTRLA_ENABLE_Msk;
 
@@ -606,3 +609,7 @@ void __attribute__((used)) SERCOM1_USART_InterruptHandler( void )
         }
     }
 }
+
+
+
+
