580bd567313ee392fd1fcfad8459a129275231e9
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 18:34:35 +0530

    Regenerated demo for cdc_com_port_single_sam_l21_xpro
    MH3-107472

--- apps/cdc_com_port_single/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/cdc_com_port_single/firmware/src/config/sam_l21_xpro/definitions.h
@@ -55,10 +55,10 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "peripheral/pm/plib_pm.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
+#include "driver/usb/usbfsv1/drv_usbfsv1.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
@@ -76,13 +76,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAML21J18B"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAML"
-#define DEVICE_SERIES		 "SAML21"
+#define DEVICE_NAME          "ATSAML21J18B"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAML"
+#define DEVICE_SERIES        "SAML21"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
@@ -194,9 +194,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  drvUSBFSV1Object;
-
     SYS_MODULE_OBJ  usbDevObject0;
+
+    SYS_MODULE_OBJ  drvUSBFSV1Object;
 
 
 } SYSTEM_OBJECTS;
