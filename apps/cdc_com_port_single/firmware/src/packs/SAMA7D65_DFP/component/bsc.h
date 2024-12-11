80ca7c56bc6f29b73bec0d0002a6d6b1ec7b0cad
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 17:30:25 +0530

    Regenerated demo for cdc_com_port_single_sam_a7d65_curiosity
    MH3-107452

--- apps/cdc_com_port_single/firmware/src/packs/SAMA7D65_DFP/component/bsc.h
+++ apps/cdc_com_port_single/firmware/src/packs/SAMA7D65_DFP/component/bsc.h
@@ -3,6 +3,8 @@
  *
  * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,34 +19,35 @@
  *
  */
 
-/* file generated from device description file (ATDF) version  */
+/*      file generated from device description file (ATDF) version None       */
 #ifndef _SAMA7D6_BSC_COMPONENT_H_
 #define _SAMA7D6_BSC_COMPONENT_H_
 
 /* ************************************************************************** */
-/*   SOFTWARE API DEFINITION FOR BSC                                          */
+/*                      SOFTWARE API DEFINITION FOR BSC                       */
 /* ************************************************************************** */
 
-/* -------- BSC_CR : (BSC Offset: 0x00) (R/W 32) Boot Sequence Controller Configuration Register -------- */
+/* -------- BSC_CR : (BSC Offset: 0x04) (R/W 32) Boot Sequence Controller Configuration Register -------- */
 #define BSC_CR_BOOT_Pos                       _UINT32_(0)                                          /* (BSC_CR) Boot Media Sequence Position */
 #define BSC_CR_BOOT_Msk                       (_UINT32_(0xFFFF) << BSC_CR_BOOT_Pos)                /* (BSC_CR) Boot Media Sequence Mask */
-#define BSC_CR_BOOT(value)                    (BSC_CR_BOOT_Msk & (_UINT32_(value) << BSC_CR_BOOT_Pos)) /* Assigment of value for BOOT in the BSC_CR register */
+#define BSC_CR_BOOT(value)                    (BSC_CR_BOOT_Msk & (_UINT32_(value) << BSC_CR_BOOT_Pos)) /* Assignment of value for BOOT in the BSC_CR register */
 #define BSC_CR_WPKEY_Pos                      _UINT32_(16)                                         /* (BSC_CR) Write Protection Key (Write-only) Position */
 #define BSC_CR_WPKEY_Msk                      (_UINT32_(0xFFFF) << BSC_CR_WPKEY_Pos)               /* (BSC_CR) Write Protection Key (Write-only) Mask */
-#define BSC_CR_WPKEY(value)                   (BSC_CR_WPKEY_Msk & (_UINT32_(value) << BSC_CR_WPKEY_Pos)) /* Assigment of value for WPKEY in the BSC_CR register */
+#define BSC_CR_WPKEY(value)                   (BSC_CR_WPKEY_Msk & (_UINT32_(value) << BSC_CR_WPKEY_Pos)) /* Assignment of value for WPKEY in the BSC_CR register */
 #define   BSC_CR_WPKEY_PASSWD_Val             _UINT32_(0x6683)                                     /* (BSC_CR) Writing any other value in this field aborts the write operation of the BOOT field. Always reads as 0.  */
-#define BSC_CR_WPKEY_PASSWD                   (BSC_CR_WPKEY_PASSWD_Val << BSC_CR_WPKEY_Pos)        /* (BSC_CR) Writing any other value in this field aborts the write operation of the BOOT field. Always reads as 0. Position  */
+#define BSC_CR_WPKEY_PASSWD                   (BSC_CR_WPKEY_PASSWD_Val << BSC_CR_WPKEY_Pos)        /* (BSC_CR) Writing any other value in this field aborts the write operation of the BOOT field. Always reads as 0. Position */
 #define BSC_CR_Msk                            _UINT32_(0xFFFFFFFF)                                 /* (BSC_CR) Register Mask  */
 
 
-/** \brief BSC register offsets definitions */
-#define BSC_CR_REG_OFST                _UINT32_(0x00)      /* (BSC_CR) Boot Sequence Controller Configuration Register Offset */
+/* BSC register offsets definitions */
+#define BSC_CR_REG_OFST                _UINT32_(0x04)      /* (BSC_CR) Boot Sequence Controller Configuration Register Offset */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
-/** \brief BSC register API structure */
+/* BSC register API structure */
 typedef struct
 {  /* Boot Sequence Controller */
-  __IO  uint32_t                       BSC_CR;             /**< Offset: 0x00 (R/W  32) Boot Sequence Controller Configuration Register */
+  __I   uint8_t                        Reserved1[0x04];
+  __IO  uint32_t                       BSC_CR;             /* Offset: 0x04 (R/W  32) Boot Sequence Controller Configuration Register */
 } bsc_registers_t;
 
 
