9c34a352813f77ebeef3006baf1a9a3def9bcdb1
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 18:10:37 +0530

    Regenerated demo for msd_basic_pic32mz_ef_sk
    MH3-107720

--- apps/msd_basic/firmware/src/config/pic32mz_ef_sk/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32mz_ef_sk/definitions.h
@@ -55,9 +55,9 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
+#include "bsp/bsp.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
-#include "bsp/bsp.h"
 #include "driver/memory/drv_memory.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
@@ -76,13 +76,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MZ2048EFH144"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MZEF"
-#define DEVICE_SERIES		 "PIC32MZ"
+#define DEVICE_NAME          "PIC32MZ2048EFH144"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MZEF"
+#define DEVICE_SERIES        "PIC32MZ"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 200000000
+#define CPU_CLOCK_FREQUENCY 200000000U
 
 // *****************************************************************************
 // *****************************************************************************
