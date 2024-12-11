f539f13bae80fb3ce9ef71c14758e16c1b1d6a5b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 16:46:12 +0530

    Regenerated demo for cdc_serial_emulator_pic32cz_ca70_curiosity_ultra
    MH3-107526

--- apps/cdc_serial_emulator/firmware/src/config/pic32cz_ca70_curiosity_ultra/configuration.h
+++ apps/cdc_serial_emulator/firmware/src/config/pic32cz_ca70_curiosity_ultra/configuration.h
@@ -116,6 +116,15 @@ extern "C" {
 #define USB_DEVICE_SOF_EVENT_ENABLE
 
 
+/* Maximum instances of CDC function driver */
+#define USB_DEVICE_CDC_INSTANCES_NUMBER                     1U
+
+
+/* CDC Transfer Queue Size for both read and
+   write. Applicable to all instances of the
+   function driver */
+#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 3U
+
 /*** USB Driver Configuration ***/
 
 /* Maximum USB driver instances */
@@ -132,15 +141,6 @@ extern "C" {
 
 /* Alignment for buffers that are submitted to USB Driver*/ 
 #define USB_ALIGN  CACHE_ALIGN
-
-/* Maximum instances of CDC function driver */
-#define USB_DEVICE_CDC_INSTANCES_NUMBER                     1U
-
-
-/* CDC Transfer Queue Size for both read and
-   write. Applicable to all instances of the
-   function driver */
-#define USB_DEVICE_CDC_QUEUE_DEPTH_COMBINED                 3U
 
 
 
