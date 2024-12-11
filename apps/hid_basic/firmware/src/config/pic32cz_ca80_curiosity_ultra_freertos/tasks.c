32b39a3ef30dba23d2af0155c2e8a17d78657a7c
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 18:04:53 +0530

    Regenerated demo for hid_basic_pic32cz_ca80_curiosity_ultra_freertos
    MH3-107564

--- apps/hid_basic/firmware/src/config/pic32cz_ca80_curiosity_ultra_freertos/tasks.c
+++ apps/hid_basic/firmware/src/config/pic32cz_ca80_curiosity_ultra_freertos/tasks.c
@@ -80,8 +80,11 @@ static void F_DRV_USBHS_Tasks0(  void *pvParameters  )
     }
 }
 
+
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
+
+
 
 static void lAPP_Tasks(  void *pvParameters  )
 {   
@@ -137,14 +140,15 @@ void SYS_Tasks ( void )
 
 
     /* Maintain the application's state machine. */
-        /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate((TaskFunction_t) lAPP_Tasks,
-                "APP_Tasks",
-                1024,
-                NULL,
-                1,
-                &xAPP_Tasks);
-
+    
+    /* Create OS Thread for APP_Tasks. */
+    (void) xTaskCreate(
+           (TaskFunction_t) lAPP_Tasks,
+           "APP_Tasks",
+           1024,
+           NULL,
+           1U ,
+           &xAPP_Tasks);
 
 
 
