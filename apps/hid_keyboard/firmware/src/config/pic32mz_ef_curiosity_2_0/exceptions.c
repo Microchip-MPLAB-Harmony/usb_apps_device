228640ba853c2525519007b6128c1b44f219c230
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 17:13:57 +0530

    Regenerated demo for hid_keyboard_pic32mz_ef_curiosity_2_0
    MH3-107656

--- apps/hid_keyboard/firmware/src/config/pic32mz_ef_curiosity_2_0/exceptions.c
+++ apps/hid_keyboard/firmware/src/config/pic32mz_ef_curiosity_2_0/exceptions.c
@@ -122,7 +122,7 @@ void __attribute__((noreturn, weak)) _general_exception_handler ( void )
     Refer to the MIPs Software User's manual */
     exception_code = ((_CP0_GET_CAUSE() & 0x0000007CU) >> 2U);
     exception_address = _CP0_GET_EPC();
-    
+
     while (true)
     {
         #if defined(__DEBUG) || defined(__DEBUG_D) && defined(__XC32)
