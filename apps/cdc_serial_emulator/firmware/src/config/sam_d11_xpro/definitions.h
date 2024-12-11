dd87e02b127f302efed5f49fdd9289e12e6855b1
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 17:35:30 +0530

    Regenerated demo for cdc_serial_emulator_sam_d11_xpro
    MH3-107536

--- apps/cdc_serial_emulator/firmware/src/config/sam_d11_xpro/definitions.h
+++ apps/cdc_serial_emulator/firmware/src/config/sam_d11_xpro/definitions.h
@@ -78,13 +78,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMD11D14AM"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMD"
-#define DEVICE_SERIES		 "SAMD11"
+#define DEVICE_NAME          "ATSAMD11D14AM"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMD"
+#define DEVICE_SERIES        "SAMD11"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
