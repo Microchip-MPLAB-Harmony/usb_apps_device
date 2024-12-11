c7035a60560221d3bc483115d089c04c2e63b187
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 16:40:41 +0530

    Regenerated demo for cdc_msd_basic_pic32cx_sg41_curiosity_ultra
    MH3-107480

--- apps/cdc_msd_basic/firmware/src/config/pic32cx_sg41_curiosity_ultra/definitions.h
+++ apps/cdc_msd_basic/firmware/src/config/pic32cx_sg41_curiosity_ultra/definitions.h
@@ -81,13 +81,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CX1025SG41128"
-#define DEVICE_ARCH			 "CORTEX-M4"
-#define DEVICE_FAMILY		 "PIC32CX"
-#define DEVICE_SERIES		 "PIC32CXSG41"
+#define DEVICE_NAME          "PIC32CX1025SG41128"
+#define DEVICE_ARCH          "CORTEX-M4"
+#define DEVICE_FAMILY        "PIC32CX"
+#define DEVICE_SERIES        "PIC32CXSG41"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 119999488
+#define CPU_CLOCK_FREQUENCY 119999488U
 
 // *****************************************************************************
 // *****************************************************************************
