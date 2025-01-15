37cb8f6eede1376ed594af247d18a627eb414c0f
Nagaraju Sreeram <nagaraju.sreeram@microchip.com>
2025-01-15 10:36:57 +0530

    [APPS]Added the demo application 'msd_multiple_luns' for pic32ck_gc01_curiosity_ultra Board. MH3-108663

---
+++ apps/msd_multiple_luns/firmware/src/packs/PIC32CK2051GC01144_DFP/instance/idau.h
@@ -0,0 +1,30 @@
+/*
+ * Instance header file for PIC32CK2051GC01144
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2024-04-02T09:16:01Z */
+#ifndef _PIC32CKGC01_IDAU_INSTANCE_
+#define _PIC32CKGC01_IDAU_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for IDAU peripheral ========== */
+#define IDAU_INSTANCE_ID                         (38)       /* Instance index for IDAU */
+#define IDAU_MCLK_ID_APB                         (69)       /* Index for IDAU APB clock */
+#define IDAU_PAC_ID                              (38)       /* Index for IDAU registers write protection */
+
+#endif /* _PIC32CKGC01_IDAU_INSTANCE_ */
