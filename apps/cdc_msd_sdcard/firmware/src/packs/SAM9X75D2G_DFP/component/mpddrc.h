0dbc45c04ca19e71b33c64f0f9b4e594c3fb601f
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-19 16:31:55 +0530

    Regenerated demo for cdc_msd_sdcard_sam_9x75_curiosity
    MH3-111412

--- apps/cdc_msd_sdcard/firmware/src/packs/SAM9X75D2G_DFP/component/mpddrc.h
+++ apps/cdc_msd_sdcard/firmware/src/packs/SAM9X75D2G_DFP/component/mpddrc.h
@@ -1,7 +1,9 @@
 /*
  * Component description for MPDDRC
  *
- * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
