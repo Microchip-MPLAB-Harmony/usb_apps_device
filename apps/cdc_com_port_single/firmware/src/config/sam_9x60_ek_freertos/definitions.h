ac919425a96273744c3c0f956de5bae969122541
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-19 16:47:17 +0530

    Regenerated demo for cdc_com_port_single_sam_9x60_ek_freertos
    MH3-111412

--- apps/cdc_com_port_single/firmware/src/config/sam_9x60_ek_freertos/definitions.h
+++ apps/cdc_com_port_single/firmware/src/config/sam_9x60_ek_freertos/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -50,8 +50,8 @@
 #include <stdbool.h>
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
-#include "bsp/bsp.h"
 #include "driver/usb/udphs/drv_usb_udphs.h"
+#include "bsp/bsp.h"
 #include "peripheral/pit/plib_pit.h"
 #include "peripheral/mmu/plib_mmu.h"
 #include "peripheral/clk/plib_clk.h"
