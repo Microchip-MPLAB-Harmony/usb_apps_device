bffb1f8bedb76dd25933a05d58b7b5d5c41f34e0
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-20 11:45:23 +0530

    Regenerated demo for msd_basic_pic32ck_gc01_curiosity_ultra_freertos
    MH3-111480

--- apps/msd_basic/firmware/src/config/pic32ck_gc01_curiosity_ultra_freertos/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32ck_gc01_curiosity_ultra_freertos/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -57,8 +57,8 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
-#include "peripheral/pm/plib_pm.h"
 #include "driver/memory/drv_memory.h"
+#include "peripheral/pm/plib_pm.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
 #include "peripheral/rtc/plib_rtc.h"
