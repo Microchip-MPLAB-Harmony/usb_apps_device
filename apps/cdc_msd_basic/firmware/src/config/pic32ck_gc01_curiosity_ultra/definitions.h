450360540e7a348d4d1ec35c3e806af37f3bd2a5
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-20 11:17:41 +0530

    Regenerated demo for cdc_msd_basic_pic32ck_gc01_curiosity_ultra
    MH3-111480

--- apps/cdc_msd_basic/firmware/src/config/pic32ck_gc01_curiosity_ultra/definitions.h
+++ apps/cdc_msd_basic/firmware/src/config/pic32ck_gc01_curiosity_ultra/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/fcw/plib_fcw.h"
 #include "driver/usb/usbhs/drv_usbhs.h"
+#include "peripheral/fcw/plib_fcw.h"
 #include "usb/usb_device_msd.h"
 #include "usb/usb_msd.h"
 #include "peripheral/evsys/plib_evsys.h"
