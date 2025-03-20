66da70fea905768122e4b6e1b405eaec5490e19b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-20 11:20:38 +0530

    Regenerated demo for cdc_msd_sdcard_pic32cm_gc_curiosity_pro
    MH3-111480

--- apps/cdc_msd_sdcard/firmware/src/config/pic32cm_gc_curiosity_pro/peripheral/sercom/spi_master/plib_sercom2_spi_master.c
+++ apps/cdc_msd_sdcard/firmware/src/config/pic32cm_gc_curiosity_pro/peripheral/sercom/spi_master/plib_sercom2_spi_master.c
@@ -62,7 +62,7 @@
 #define SERCOM2_SPIM_BAUD_VALUE         (23UL)
 
 /*Global object to save SPI Exchange related data  */
-volatile static SPI_OBJECT sercom2SPIObj;
+static volatile SPI_OBJECT sercom2SPIObj;
 
 // *****************************************************************************
 // *****************************************************************************
