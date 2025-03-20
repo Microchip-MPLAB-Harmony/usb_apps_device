1bc79179c28fded4ea5ca4913f087f46881882f6
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-20 11:17:44 +0530

    Regenerated demo for cdc_serial_emulator_msd_pic32ck_gc01_curiosity_ultra
    MH3-111480

--- apps/cdc_serial_emulator_msd/firmware/src/config/pic32ck_gc01_curiosity_ultra/definitions.h
+++ apps/cdc_serial_emulator_msd/firmware/src/config/pic32ck_gc01_curiosity_ultra/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/fcw/plib_fcw.h"
 #include "driver/usb/usbhs/drv_usbhs.h"
+#include "peripheral/fcw/plib_fcw.h"
 #include "usb/usb_device_msd.h"
 #include "usb/usb_msd.h"
 #include "peripheral/evsys/plib_evsys.h"
@@ -58,15 +58,15 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "driver/memory/drv_memory.h"
-#include "peripheral/sercom/usart/plib_sercom5_usart.h"
 #include "peripheral/pm/plib_pm.h"
+#include "peripheral/sercom/usart/plib_sercom5_usart.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_device.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
 #include "driver/memory/drv_memory_fcw.h"
-#include "driver/usart/drv_usart.h"
 #include "bsp/bsp.h"
+#include "driver/usart/drv_usart.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
